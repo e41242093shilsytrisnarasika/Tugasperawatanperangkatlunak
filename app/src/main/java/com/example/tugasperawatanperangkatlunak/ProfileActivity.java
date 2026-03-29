8e6bf35b120ae338c4cd539acc48afafe97a45c9
Shilsy <e41242093@student.polije.ac.id>
2026-03-29 18:59:45 +0700

    docs: analyze system errors impact and user experience issues

--- app/src/main/java/com/example/tugasperawatanperangkatlunak/ProfileActivity.java
+++ app/src/main/java/com/example/tugasperawatanperangkatlunak/ProfileActivity.java
@@ -30,7 +30,7 @@ public class ProfileActivity extends AppCompatActivity {
 
         // Tombol edit: sengaja tidak melakukan apa-apa
         btnEdit.setOnClickListener(v -> {
-            // Kosong, tidak ada aksi *trsna
+            // Kosong, tidak ada aksi *trsnaR
         });
     }
 }
